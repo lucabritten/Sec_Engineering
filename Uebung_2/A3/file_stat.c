b254f32dc10b0c5a695917e9a7b8f321218ca5af
Luca Britten <lucabritten17@icloud.com>
2026-04-28 19:49:23 +0200

    Implemented task 3

--- Uebung_2/A3/file_stat.c
+++ Uebung_2/A3/file_stat.c
@@ -2,6 +2,7 @@
 #include <sys/stat.h>
 #include <unistd.h>
 #include <stdio.h>
+#include <time.h>
 #include <pwd.h>
 
 int main(int argc, char *argv[]) {
@@ -10,20 +11,40 @@ int main(int argc, char *argv[]) {
         return 1;
     }
 
-    printf("You entered %d arguments\n", argc);
-    struct stat file_arr[argc];
+    for (int i = 1; i < argc; i++) {
+        struct stat file;
 
-    for (int i = 1;i < argc; i++) { 
+        if (lstat(argv[i], &file) == -1) {
+            perror("lstat failed");
+            continue;
+        }
+
         printf("[File no. %d]: %s\n", i, argv[i]);
 
-        lstat(argv[i], &file_arr[i]);
-        struct passwd *p;
-        p = getpwuid(file_arr[i].st_uid);
+        printf("\tFiletype: ");
+        if (S_ISREG(file.st_mode)) printf("regular file\n");
+        else if (S_ISDIR(file.st_mode)) printf("directory\n");
+        else if (S_ISFIFO(file.st_mode)) printf("pipe\n");
+        else if (S_ISSOCK(file.st_mode)) printf("socket\n");
+        else if (S_ISCHR(file.st_mode)) printf("char device\n");
+        else if (S_ISLNK(file.st_mode)) printf("symbolic link\n");
+        else printf("unknown\n");
 
+        printf("\tUID: %d\n", file.st_uid);
 
-        printf("\tFiletype: %hu\n", file_arr[i].st_mode);
-        printf("\tUID: %d", file_arr[i].st_uid);
-        printf("Corresponding user: %s\n", p->pw_name);
+        struct passwd *p = getpwuid(file.st_uid);
+        if (p)
+            printf("\tCorresponding user: %s\n", p->pw_name);
+        else
+            printf("\tCorresponding user: unknown\n");
+
+        printf("\tGID: %d\n", file.st_gid);
+        printf("\tZugriffsbits: %o\n", file.st_mode & 0777);
+
+        printf("\tLast access: %s\n", ctime(&file.st_atime));
+        printf("\tLast inode change: %s\n", ctime(&file.st_ctime));
+        printf("\tLast file change: %s\n", ctime(&file.st_mtime));
+        printf("\tFile creation: %s\n", ctime(&file.st_birthtime));
     }
     return 0;
 }
